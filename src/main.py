c0efd6bd982c79200f64f68f5b0d2db6b1e2efdd
xRetry <marcoschwarz.at@gmail.com>
2023-10-19 20:04:51 +0200

    Fix script file

--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ class SimSettings:
     job_name: str
     script_path: str
     input_path: str
+    num_cpus: int
     num_gpus: int
-    mem_size_gb: int
+    mem_size_mb: int
     scratch_path: str
 
 def exec_command(command: str) -> subprocess.CompletedProcess[bytes]:
@@ -39,25 +40,44 @@ def replace_all(text: str, replace: Dict[str, str|Callable[[], str]]) -> str:
         text = text.replace(old_str, new_str)
     return text
 
+def create_job_command(settings: SimSettings) -> str:
+    return f"mdb.Job(name='{settings.job_name}', model='Model-1', description='', type=ANALYSIS, 
+        atTime=None, waitMinutes=0, waitHours=0, queue=None, memory={settings.mem_size_mb}, 
+        memoryUnits=MEGA_BYTES, getMemoryFromAnalysis=True, 
+        explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, echoPrint=OFF, 
+        modelPrint=OFF, contactPrint=OFF, historyPrint=OFF, userSubroutine='', 
+        scratch='{settings.scratch_path}', resultsFormat=ODB, numThreadsPerMpiProcess=1, 
+        multiprocessingMode=DEFAULT, numCpus={settings.num_cpus}, numDomains=4, numGPUs={settings.num_gpus})"
+
+def create_job_submit(settings: SimSettings) -> str:
+    return f"mdb.jobs['{settings.job_name}'].submit(consistencyChecking=OFF)"
+
 def run_sim(settings: SimSettings):
-    # TODO(marco): Set correct input file path in script
-    output = exec_command(f'abaqus cae noGUI={settings.script_path}')
+    # TODO(marco): Set correct file paths in script
+
+    template_path = f'templates/{settings.job_name}.py'
+    script_path = f'scripts/{settings.job_name}.py'
+     
+    script = read_file(template_path)
+    re.sub("mdb\.Job\((.|\n)*\)", create_job_command(settings), script)
+    re.sub("mdb\.jobs\[.*submit\(.*\)", create_job_submit(settings), script)
+    write_file(script_path, script)
+
+    output = exec_command(f'abaqus cae noGUI={script_path}')
     if output.returncode != 0:
         print("Could not execute journal file!")
         print(output.stderr)
         return
-
-    sim_command = f'abaqus job={settings.job_name} gpus={settings.num_gpus} memory={settings.mem_size_gb} scratch={settings.scratch_path} input={settings.input_path}'
-    output = exec_command(sim_command)
 
 def main() -> None:
     settings = SimSettings(
         job_name='test',
         script_path='journal/test.py',
         input_path='test.inp',
-        num_gpus=4,
-        mem_size_gb=4,
-        scratch_path='scratch',
+        num_cpus=4,
+        num_gpus=0,
+        mem_size_mb=4,
+        scratch_path='',
     )
     run_sim(settings)
 
