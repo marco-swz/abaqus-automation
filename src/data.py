7789133d90ae3235e2ac1c1dd8d3ef0b55327983
xRetry <marcoschwarz.at@gmail.com>
2023-11-16 18:44:34 +0100

    Do data extraction without CAE

--- src/data.py
+++ src/data.py
@@ -1,20 +1,35 @@
-from abaqus import *
-from abaqusConstants import *
+from odbAccess import *
+#from abaqusConstants import *
+#from symbolicConstants import *
 import numpy as np
+import json
             
-odb = session.openOdb(name='test.odb')
+odb = openOdb('data.odb')
 
 def get_frames(odb):
+    json.dumps('data.json')
     for step_name in odb.steps.keys():
         frames = odb.steps[step_name].frames
 
-        values = []
-        for frame in frames:
-            for output in frame.fieldOutputs:
-                field = np.zeros((output.dim, output.dim2, 3))
-                for val in output.values:
-                    field[,,0] = val.mises
-                values.append(field)
+        print('Processing step `{}` ...'.format(step_name))
+        frameMap = {}
+        for i, frame in enumerate(frames):
+            field = {}
+            for field_output_key in frame.fieldOutputs.keys():
+                print('\tProcessing field output `{}` ...'.format(field_output_key))
+                field_output = frame.fieldOutputs[field_output_key]
+                values = np.zeros((len(field_output.values), 3))
 
+                for i, val in enumerate(field_output.values):
+                    values[i,0] = val.mises
+                    values[i,1] = val.inv3
+                    values[i,2] = val.press
 
+                field[field_output_key] = values
+
+            frameMap[i] = field
+
+    # TODO(marco): Store data
+
+get_frames(odb)
 
