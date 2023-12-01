6cc91e5fbb625a43067f4185c7d66d9d7471c236
xRetry <marcoschwarz.at@gmail.com>
2023-12-01 20:48:29 +0100

    Finish data extraction

--- src/data.py
+++ src/data.py
@@ -1,35 +1,49 @@
+from __future__ import print_function
 from odbAccess import *
-#from abaqusConstants import *
-#from symbolicConstants import *
 import numpy as np
 import json
             
 odb = openOdb('data.odb')
 
 def get_frames(odb):
-    json.dumps('data.json')
+    stepMap = {}
     for step_name in odb.steps.keys():
         frames = odb.steps[step_name].frames
 
-        print('Processing step `{}` ...'.format(step_name))
         frameMap = {}
         for i, frame in enumerate(frames):
+            print(' '*100, end='\r')
+            print('Processing step `{}` {}/{} ... '.format(step_name, i+1, len(frames)), end='\r')
+
             field = {}
             for field_output_key in frame.fieldOutputs.keys():
-                print('\tProcessing field output `{}` ...'.format(field_output_key))
                 field_output = frame.fieldOutputs[field_output_key]
-                values = np.zeros((len(field_output.values), 3))
 
+                values = []
                 for i, val in enumerate(field_output.values):
-                    values[i,0] = val.mises
-                    values[i,1] = val.inv3
-                    values[i,2] = val.press
+                    if val.precision == SINGLE_PRECISION:
+                        data = val.data
+                    else:
+                        data = val.dataDouble
+
+                    if (isinstance(data, np.ndarray)):
+                        data = data.tolist()
+
+                    values.append(data)
 
                 field[field_output_key] = values
-
             frameMap[i] = field
+        stepMap[step_name] = frameMap
 
-    # TODO(marco): Store data
+        print('Processing step `{}` {}/{} ... done'.format(step_name, len(frames), len(frames)), end='\r')
+        print('')
+
+    print('Storing data ... ', end='')
+
+    with open('data.json', 'w') as fp:
+        json.dump(stepMap, fp)
+            
+    print('done')
 
 get_frames(odb)
 
