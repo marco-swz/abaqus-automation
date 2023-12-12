8700b2a61ba7a509d3727604037530ef4f84922e
xRetry <marcoschwarz.at@gmail.com>
2023-12-12 16:20:50 +0100

    Add pyarrow storage

--- src/data_conversion.py
+++ src/data_conversion.py
@@ -1,5 +1,7 @@
 import pyarrow as pa
 import pyarrow.dataset as ds
+import pyarrow.parquet as pq
+from pyarrow.interchange import from_dataframe
 import json
 import polars as pl
 import numpy as np
@@ -15,13 +17,19 @@ class Dataset:
     types: ...
     nodes: ...
 
+    def write_parquet(self, dir: str):
+        self.steps.write_parquet(dir+'/steps.parquet')
+        self.frames.write_parquet(dir+'/frames.parquet')
+        self.types.write_parquet(dir+'/types.parquet')
+        self.nodes.write_parquet(dir+'/nodes.parquet')
+
 def read_data(path: str) -> dict:
     file = open(path)
     data = json.load(file)
     file.close()
     return data
 
-def to_table(data: dict):
+def to_table(data: dict) -> pl.DataFrame:
     table = [];
     for step_name, step in data.items():
         for frame_num, frame in step.items():
@@ -39,9 +47,10 @@ def to_table(data: dict):
                     ] + node_padded
 
                     table.append(row)
-    return table
+    col_names = ['step_name', 'frame_num', 'val_type', 'val1', 'val2', 'val3', 'val4', 'val5', 'val6']
+    return pl.DataFrame(table, col_names)
 
-def to_dataset(data):
+def to_dataset(data) -> Dataset:
     table_steps = []
     table_nodes = []
     table_frames = []
@@ -70,7 +79,7 @@ def to_dataset(data):
                     table_nodes.append(node_padded)
 
     table_types = [[id, type_name] for type_name, id in types_unique.items()]
-    print(table_nodes[0])
+
     return Dataset(
         steps=pl.DataFrame(table_steps, ['step_id', 'step_name']),
         types=pl.DataFrame(table_types, ['type_id', 'type_name']),
@@ -84,24 +93,22 @@ def to_dataset2(data: dict):
     ds.write_dataset(table, partitioning=part, base_dir='data/arrow', format="parquet")
     #ds.write_dataset(arr, 'data/arrow', format="parquet")
 
-def save_table_as_parquet(table: list, path: str):
-    df = pl.DataFrame(table)
-    df.write_parquet(path)
+def save_table_as_parquet(table: pl.DataFrame, path: str):
+    table.write_parquet(path)
 
-def save_table_as_csv(table: list, path: str):
-    df = pl.DataFrame(table)
-    df.write_csv(path)
+def save_table_as_csv(table: pl.DataFrame, path: str):
+    table.write_csv(path)
 
-def save_table_as_sqlite(table: list, path: str):
+def save_table_as_sqlite(table: pl.DataFrame, path: str):
     conn = sqlite3.connect(path)
 
     with conn:
         sql = '''
             CREATE TABLE IF NOT EXISTS data(
                 id integer PRIMARY KEY,
-                step text,
-                frame text,
-                type text,
+                step_name text,
+                frame_num text,
+                type_name text,
                 val1 real,
                 val2 real,
                 val3 real,
@@ -117,9 +124,9 @@ def save_table_as_sqlite(table: list, path: str):
         for row in table:
             sql = '''
                 INSERT INTO data(
-                    step,
-                    frame,
-                    type,
+                    step_name,
+                    frame_num,
+                    type_name,
                     val1,
                     val2,
                     val3,
@@ -145,15 +152,31 @@ def save_as_pickle(data, path):
     pickle.dump(data, file)
     file.close()
 
+def save_table_as_arrow_table(table, path: str):
+    table = from_dataframe(table)
+    pq.write_table(table, path)
+
+def save_table_as_arrow_dataset(table, path: str):
+    table = from_dataframe(table)
+    part = ds.partitioning(pa.schema([
+        ('step_name', pa.large_string()),
+        ('frame_num', pa.int32()),
+    ]), flavor=None)
+
+    ds.write_dataset(table, path, format='parquet', partitioning=part)
+
+
 if __name__ == '__main__':
     data = read_data('data/data.json')
-    #table = to_table(data)
+    table = to_table(data)
     #save_table_as_csv(table, 'data/data.csv')
     #save_table_as_parquet(table, 'data/data.parquet')
     #save_as_pickle(table, 'data/table.pickle')
     #save_as_pickle(data, 'data/data.pickle')
     #save_as_compressed(data, 'data/data.gz')
     #save_table_as_sqlite(table, 'data/data.sqlite')
+    #save_table_as_arrow_table(table, 'data/arrow-table')
+    save_table_as_arrow_dataset(table, 'data/arrow-dataset')
 
-    dataset = to_dataset(data)
-    print(dataset)
+    #dataset = to_dataset(data)
+    #dataset.to_parquet('data/polars')
