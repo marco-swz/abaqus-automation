ea833aef06c2a60d14b973e4a4d9cb606c6ddcd6
xRetry <marcoschwarz.at@gmail.com>
2023-12-16 15:27:07 +0100

    Finish benchmarks

--- src/data_conversion.py
+++ src/data_conversion.py
@@ -11,6 +11,8 @@ import gzip
 import sqlite3
 from dataclasses import dataclass
 
+FRAME_REPEATS = 10
+
 @dataclass
 class Dataset:
     steps: ... 
@@ -18,30 +20,37 @@ class Dataset:
     types: ...
     nodes: ...
 
-def read_data(path: str) -> dict:
+def read_json(path: str) -> dict:
     file = open(path)
     data = json.load(file)
     file.close()
     return data
 
+def write_json(obj: dict, path: str):
+    file = open(path, 'w')
+    json.dump(obj, file)
+    file.close()
+
 def to_table(data: dict) -> pl.DataFrame:
     table = [];
     for step_name, step in data.items():
         for frame_num, frame in step.items():
-            for val_type, nodes in frame.items():
-                for node in nodes:
-                    node_padded = [np.NaN]*6
-                    if isinstance(node, float):
-                        node = [node]
-                    node_padded[:len(node)] = node
+            for frame_repeats in range(FRAME_REPEATS):
+                frame_num_repeat = frame_num + str(frame_repeats)
+                for val_type, nodes in frame.items():
+                    for node in nodes:
+                        node_padded = [np.NaN]*6
+                        if isinstance(node, float):
+                            node = [node]
+                        node_padded[:len(node)] = node
 
-                    row = [
-                        step_name,
-                        frame_num,
-                        val_type,
-                    ] + node_padded
+                        row = [
+                            step_name,
+                            frame_num_repeat,
+                            val_type,
+                        ] + node_padded
 
-                    table.append(row)
+                        table.append(row)
     col_names = ['step_name', 'frame_num', 'val_type', 'val1', 'val2', 'val3', 'val4', 'val5', 'val6']
     return pl.DataFrame(table, col_names)
 
@@ -57,25 +66,27 @@ def to_dataset(data) -> Dataset:
         table_steps.append([step_id, step_name])
         step = data[step_name]
 
-        for frame_num in step.keys():
-            frame_id += 1
-            table_frames.append([frame_id, step_id, frame_num])
+        for frame_num, types in step.items():
+            for frame_repeats in range(FRAME_REPEATS):
+                frame_num_repeat = frame_num + str(frame_repeats)
+                frame_id += 1
+                table_frames.append([frame_id, step_id, frame_num_repeat])
 
-            for node_type, nodes in step[frame_num].items():
-                type_id = types_unique.get(node_type)
-                if type_id is None:
-                    types_id += 1
-                    types_unique[node_type] = types_id
-                    type_id = types_id
+                for node_type, nodes in types.items():
+                    type_id = types_unique.get(node_type)
+                    if type_id is None:
+                        types_id += 1
+                        types_unique[node_type] = types_id
+                        type_id = types_id
 
-                for node in nodes:
-                    node_id += 1
-                    node_padded = [node_id, type_id, frame_id] + [np.NaN]*5
-                    if isinstance(node, float):
-                        node = [node]
-                    node_padded[3:len(node)] = node
+                    for node in nodes:
+                        node_id += 1
+                        node_padded = [node_id, type_id, frame_id] + [np.NaN]*5
+                        if isinstance(node, float):
+                            node = [node]
+                        node_padded[3:len(node)] = node
 
-                    table_nodes.append(node_padded)
+                        table_nodes.append(node_padded)
 
     table_types = [[id, type_name] for type_name, id in types_unique.items()]
 
@@ -87,9 +98,11 @@ def to_dataset(data) -> Dataset:
     )
 
 class ParquetTable:
+    name = 'Parquet single'
+
     @staticmethod
     def convert(in_path: str, out_path: str):
-        data = read_data(in_path)
+        data = read_json(in_path)
         table = to_table(data)
         table.write_parquet(out_path)
 
@@ -103,9 +116,11 @@ class ParquetTable:
         )
 
 class ParquetDataset:
+    name = "Parquet multiple"
+
     @staticmethod
     def convert(in_path: str, out_dir: str):
-        data = read_data(in_path)
+        data = read_json(in_path)
         dataset = to_dataset(data)
         dataset.steps.write_parquet(out_dir+'/steps.parquet')
         dataset.frames.write_parquet(out_dir+'/frames.parquet')
@@ -124,9 +139,11 @@ class ParquetDataset:
             )
 
 class CsvTable:
+    name = 'CSV single'
+
     @staticmethod
     def convert(in_path: str, out_path: str):
-        data = read_data(in_path)
+        data = read_json(in_path)
         table = to_table(data) 
         table.write_csv(out_path)
 
@@ -141,9 +158,11 @@ class CsvTable:
             )
 
 class SqliteTable:
+    name = 'Sqlite single'
+
     @staticmethod
     def convert(in_path: str, out_path: str):
-        data = read_data(in_path)
+        data = read_json(in_path)
         table = to_table(data) 
         conn = sqlite3.connect(out_path)
 
@@ -215,9 +234,11 @@ class SqliteTable:
         samples = pl.DataFrame(np.array(samples))
 
 class SqliteDataset:
+    name = 'Sqlite multiple'
+
     @staticmethod
     def convert(in_path: str, out_path: str):
-        data = read_data(in_path)
+        data = read_json(in_path)
         dataset = to_dataset(data) 
         conn = sqlite3.connect(out_path)
 
@@ -354,9 +375,11 @@ def save_as_compressed(data, path: str):
     file.close()
 
 class ArrowTable:
+    name = "Arrow single"
+
     @staticmethod
     def convert(in_path: str, out_path: str):
-        data = read_data(in_path)
+        data = read_json(in_path)
         table = to_table(data)
         table = from_dataframe(table)
         pq.write_table(table, out_path)
@@ -372,9 +395,11 @@ class ArrowTable:
 
 
 class ArrowTablePart:
+    name = "Arrow partitioned"
+
     @staticmethod
     def convert(in_path: str, out_path: str):
-        data = read_data(in_path)
+        data = read_json(in_path)
         table = to_table(data)
         table = from_dataframe(table)
         part = ds.partitioning(pa.schema([
@@ -399,7 +424,7 @@ class ArrowTablePart:
         samples = pl.from_arrow(samples.to_table())
 
 def read_batch(input_file:str, path: str, storage):
-    data = read_data(input_file)
+    data = read_json(input_file)
     dataset = to_dataset(data)
     num_reads = 10
     seeds = np.arange(0, num_reads)
@@ -412,29 +437,37 @@ def read_batch(input_file:str, path: str, storage):
         storage.read_batch(path, steps, types)
         timings[i] = time.time() - start
 
-    print(timings)
-    print(timings.mean())
+    print(f'Benchmark done for `{storage.name}`')
+    print(f'\tMean: {timings.mean()}')
+    print(f'\tStd: {timings.std()}')
+
+    stats = dict(
+        storage_name=storage.name,
+        read_durations_s=list(timings),
+        frame_repeats=FRAME_REPEATS,
+    )
+    write_json(stats, f'benchmarks/{storage.name.lower().replace(" ", "-")}-{FRAME_REPEATS}.json')
 
 
 if __name__ == '__main__':
     input_file = 'data/data.json'
-    #CsvTable.convert(input_file, 'data/table.csv')
+    CsvTable.convert(input_file, 'data/table.csv')
     read_batch(input_file, 'data/table.csv', CsvTable)
 
-    #ParquetTable.convert(input_file, 'data/table.parquet')
+    ParquetTable.convert(input_file, 'data/table.parquet')
     read_batch(input_file, 'data/table.parquet', ParquetTable)
 
-    #SqliteTable.convert(input_file, 'data/table.sqlite')
+    SqliteTable.convert(input_file, 'data/table.sqlite')
     read_batch(input_file, 'data/table.sqlite', SqliteTable)
 
-    #SqliteDataset.convert(input_file, 'data/dataset.sqlite')
+    SqliteDataset.convert(input_file, 'data/dataset.sqlite')
     read_batch(input_file, 'data/dataset.sqlite', SqliteDataset)
 
-    #ParquetDataset.convert(input_file, 'data/parquet')
+    ParquetDataset.convert(input_file, 'data/parquet')
     read_batch(input_file, 'data/parquet', ParquetDataset)
 
-    #ArrowTablePart.convert(input_file, 'data/arrow-table-part')
+    ArrowTablePart.convert(input_file, 'data/arrow-table-part')
     read_batch(input_file, 'data/arrow-table-part', ArrowTablePart)
 
-    #ArrowTable.convert(input_file, 'data/arrow-table')
+    ArrowTable.convert(input_file, 'data/arrow-table')
     read_batch(input_file, 'data/arrow-table', ArrowTable)
